4c5551be9f962878ec5b0b5d52c6391c1ed4d392
Muhammad Kamal Fergany <mohammedkamalfergany@yahoo.com>
2018-07-18 11:44:10 +0200

    Add Basic Routing System
    Provide routing and navigation using different views

--- client/main.js
+++ client/main.js
@@ -2,3 +2,24 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+import './view/dashboard.html';
+import './view/profile.html';
+import './view/case.html';
+
+FlowRouter.route('/', {
+	action() {
+		BlazeLayout.render('main', { main: 'dashboard' });
+	}
+});
+
+FlowRouter.route('/profile', {
+	action() {
+		BlazeLayout.render('main', { main: 'profile' });
+	}
+});
+
+FlowRouter.route('/case', {
+	action() {
+		BlazeLayout.render('main', { main: 'case' });
+	}
+});
