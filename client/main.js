b80b81784ce7bb2770046cd9ba226502edce7fa1
Muhammad Kamal Fergany <mohammedkamalfergany@yahoo.com>
2018-07-18 17:23:38 +0200

    Add Basic Case Form
    Provide basic case addition form into database.

--- client/main.js
+++ client/main.js
@@ -1,5 +1,6 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import { Cases } from '../lib/collection/collections.js';
 
 import './main.html';
 import './view/dashboard.html';
@@ -30,5 +31,28 @@ FlowRouter.route('/case', {
 Template.nav.helpers({
   activeLink(link) {
     return (FlowRouter._current.path === link) ? "active" : "";
+  }
+});
+
+Template.case.events({
+  'submit #add-case': function(e) {
+    e.preventDefault();
+    var caseName = e.target.name.value;
+    Meteor.call('insertCase', caseName);
+    e.target.reset();
+  },
+
+  'click .case-remove': function(e) {
+    Meteor.call('removeCase', this._id);
+  }
+});
+
+Template.case.helpers({
+  cases() {
+    return Cases.find().fetch().length > 0;
+  },
+
+  caseList() {
+    return Cases.find().fetch();
   }
 });
