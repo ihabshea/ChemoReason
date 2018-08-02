05e77057b6bdbd4f0d080d424f8956ff24822642
Khaled Mohamed <kaldreno@gmail.com>
2018-08-02 14:59:55 +0200

    Linked The Doctor and Patient Review Pages into Main HTML & JS files

--- client/main.js
+++ client/main.js
@@ -1,11 +1,13 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-import { Cases } from '../lib/collection/collections.js';
+import { Cases, Reviews } from '../lib/collection/collections.js';
 
 import './main.html';
 import './view/dashboard.html';
 import './view/profile.html';
 import './view/case.html';
+import './view/DoctorReview.html';
+import './view/PatientReview.html';
 
 FlowRouter.route('/', {
 	action() {
@@ -25,6 +27,20 @@ FlowRouter.route('/case', {
 	action() {
     BlazeLayout.reset();
 		BlazeLayout.render('main', { nav:'nav', main: 'case' });
+	}
+});
+
+FlowRouter.route('/DoctorReview', {
+	action() {
+    BlazeLayout.reset();
+		BlazeLayout.render('main', { nav:'nav', main: 'DoctorReview' });
+	}
+});
+
+FlowRouter.route('/PatientReview', {
+	action() {
+    BlazeLayout.reset();
+		BlazeLayout.render('main', { nav:'nav', main: 'PatientReview' });
 	}
 });
 
@@ -61,4 +77,37 @@ Template.profile.helpers({
   getEmail() {
     return Meteor.users.findOne({ _id: Meteor.userId }).emails[0].address;
   }
+});
+
+Template.DoctorReview.events({
+	'submit #doctor-review-form': function(event) {
+		// this.target refers to the current form
+		// so we can invoke form-related methods.
+
+		// prevent page redirection
+		event.preventDefault();
+
+		// check if any field is empty (by referencing them from this.target)
+		if (event.target.firstName.value == '' ||
+		event.target.lastName.value == '' ||
+		event.target.commentField.value == '' ||
+	  event.target.querySelector(':checked') == null) {
+			alert('Please fill all information first');
+			return;
+		}
+
+		var name = event.target.firstName.value +  ' ' + event.target.lastName.value;
+		var comment = event.target.commentField.value;
+		var rating;
+		event.target.querySelectorAll('input[name="radio"]').forEach(function(e) {
+			if (e.checked)
+				rating = e.value;
+		});
+
+		Meteor.call('addReview', name, comment, rating);
+
+
+		// so we can empty other fields.
+		event.target.reset();
+	}
 });
