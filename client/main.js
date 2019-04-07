49145b4ea9e6ab52d56d3dd2c31d26bf336f7ebd
mohkamfer <mohammedkamalfergany@yahoo.com>
2019-04-08 01:29:00 +0200

    Remove Accounts-ui & Format Code

--- client/main.js
+++ client/main.js
@@ -11,76 +11,76 @@ import './view/PatientReview.html';
 
 FlowRouter.route('/', {
 	action() {
-    BlazeLayout.reset();
-		BlazeLayout.render('main', { nav:'nav', main: 'dashboard' });
+		BlazeLayout.reset();
+		BlazeLayout.render('main', { nav: 'nav', main: 'dashboard' });
 	}
 });
 
 FlowRouter.route('/profile', {
 	action() {
-    BlazeLayout.reset();
-		BlazeLayout.render('main', { nav:'nav', main: 'profile' });
+		BlazeLayout.reset();
+		BlazeLayout.render('main', { nav: 'nav', main: 'profile' });
 	}
 });
 
 FlowRouter.route('/case', {
 	action() {
-    BlazeLayout.reset();
-		BlazeLayout.render('main', { nav:'nav', main: 'case' });
+		BlazeLayout.reset();
+		BlazeLayout.render('main', { nav: 'nav', main: 'case' });
 	}
 });
 
 FlowRouter.route('/DoctorReview', {
 	action() {
-    BlazeLayout.reset();
-		BlazeLayout.render('main', { nav:'nav', main: 'DoctorReview' });
+		BlazeLayout.reset();
+		BlazeLayout.render('main', { nav: 'nav', main: 'DoctorReview' });
 	}
 });
 
 FlowRouter.route('/PatientReview', {
 	action() {
-    BlazeLayout.reset();
-		BlazeLayout.render('main', { nav:'nav', main: 'PatientReview' });
+		BlazeLayout.reset();
+		BlazeLayout.render('main', { nav: 'nav', main: 'PatientReview' });
 	}
 });
 
 Template.nav.helpers({
-  activeLink(link) {
-    return (FlowRouter._current.path === link) ? "active" : "";
-  }
+	activeLink(link) {
+		return (FlowRouter._current.path === link) ? "active" : "";
+	}
 });
 
 Template.case.events({
-  'submit #add-case': function(e) {
-    e.preventDefault();
-    var caseName = e.target.name.value;
-    Meteor.call('insertCase', caseName);
-    e.target.reset();
-  },
+	'submit #add-case': function (e) {
+		e.preventDefault();
+		var caseName = e.target.name.value;
+		Meteor.call('insertCase', caseName);
+		e.target.reset();
+	},
 
-  'click .case-remove': function(e) {
-    Meteor.call('removeCase', this._id);
-  }
+	'click .case-remove': function (e) {
+		Meteor.call('removeCase', this._id);
+	}
 });
 
 Template.case.helpers({
-  cases() {
-    return Cases.find().fetch().length > 0;
-  },
+	cases() {
+		return Cases.find().fetch().length > 0;
+	},
 
-  caseList() {
-    return Cases.find().fetch();
-  }
+	caseList() {
+		return Cases.find().fetch();
+	}
 });
 
 Template.profile.helpers({
-  getEmail() {
-    return Meteor.users.findOne({ _id: Meteor.userId }).emails[0].address;
-  }
+	getEmail() {
+		return Meteor.users.findOne({ _id: Meteor.userId }).emails[0].address;
+	}
 });
 
 Template.DoctorReview.events({
-	'submit #doctor-review-form': function(event) {
+	'submit #doctor-review-form': function (event) {
 		// this.target refers to the current form
 		// so we can invoke form-related methods.
 
@@ -89,17 +89,17 @@ Template.DoctorReview.events({
 
 		// check if any field is empty (by referencing them from this.target)
 		if (event.target.firstName.value == '' ||
-		event.target.lastName.value == '' ||
-		event.target.commentField.value == '' ||
-	  event.target.querySelector(':checked') == null) {
+			event.target.lastName.value == '' ||
+			event.target.commentField.value == '' ||
+			event.target.querySelector(':checked') == null) {
 			alert('Please fill all information first');
 			return;
 		}
 
-		var name = event.target.firstName.value +  ' ' + event.target.lastName.value;
+		var name = event.target.firstName.value + ' ' + event.target.lastName.value;
 		var comment = event.target.commentField.value;
 		var rating;
-		event.target.querySelectorAll('input[name="radio"]').forEach(function(e) {
+		event.target.querySelectorAll('input[name="radio"]').forEach(function (e) {
 			if (e.checked)
 				rating = e.value;
 		});
